ecf3e3e6919b034ca7d5a9fbd9f2cc6074ae69a6
alexeyzer <alexeyz.zudin@yandex.ru>
2022-05-08 17:24:59 +0300

    debug

--- internal/product_serivce/list_products_by_photo.go
+++ internal/product_serivce/list_products_by_photo.go
@@ -3,9 +3,11 @@ package product_serivce
 import (
 	"context"
 	desc "github.com/alexeyzer/product-api/pb/api/product/v1"
+	log "github.com/sirupsen/logrus"
 )
 
 func (s *ProductApiServiceServer) ListProductsByPhoto(ctx context.Context, req *desc.ListProductsByPhotoRequest) (*desc.ListProductsResponse, error) {
+	log.Printf("byte: %v", req.GetImage())
 	res, err := s.productService.ListProductsByPhoto(ctx, req.GetImage())
 	if err != nil {
 		return nil, err
